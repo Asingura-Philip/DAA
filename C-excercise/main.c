70f4712026d7b28d895af95925504ffbbd0f743a
Asingura Philip <asinguraphilip@gmail.com>
2024-09-12 19:58:46 +0300

    c files

--- C-excercise/main.c
+++ C-excercise/main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*write an algorithm to find the maximum element in an array 
+	impliment the algorith with C code
+ */
 
+
+/*
+psuedo code
+get array
+loop through the elements in an array 
+assign first element to variable
+if next element is bigger than variable,re-assign variable to that element
+if its smaller, check the next element
+print out the variable
+
+
+*/
 int main() {
-	printf("hello");
+	
+	int myArray[] = {2,4,6,3,7,5};
+	int length = sizeof(myArray) / sizeof(myArray[0]);
+ 	printf(length);
+    //printf("The length of the array is: %d\n", length);
+    int i;
+    for(i=0;i<length;i++){
+    	printf(myArray[i]);
+	};
+	
 	return 0;
 }
