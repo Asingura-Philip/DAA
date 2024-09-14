97f488069bbfc21a15f9e6cd10f3b4d2c9b9b0ae
Asingura Philip <asinguraphilip@gmail.com>
2024-09-14 08:51:00 +0300

    algorithm and psedo code

--- C-excercise/main.c
+++ C-excercise/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /*write an algorithm to find the maximum element in an array 
-	impliment the algorith with C code
+	impliment the algorithm with C code
  */
 
 
@@ -17,16 +17,53 @@ print out the variable
 
 
 */
-int main() {
-	
-	int myArray[] = {2,4,6,3,7,5};
-	int length = sizeof(myArray) / sizeof(myArray[0]);
- 	printf(length);
-    //printf("The length of the array is: %d\n", length);
+//int main() {
+//	
+//	int myArray[] = {2,4,6,3,7,5};
+//	int length = sizeof(myArray) / sizeof(myArray[0]);
+// 	printf("%d",length);
+// 	int val = length
+//    printf("The length of the array is: %d\n", length);
+//    int i;
+//    for(i=0;i<length;i++){
+//    	printf(myArray[i]);
+//	};
+//	
+//	return 0;
+//}
+#include <stdio.h>
+
+// Function to find the maximum element in an array
+int findMax(int arr[], int size) {
+    int max = arr[0]; // Initialize max to the first element
     int i;
-    for(i=0;i<length;i++){
-    	printf(myArray[i]);
-	};
-	
-	return 0;
+    for (i = 1; i < size; i++) { // Start from the second element
+        if (arr[i] > max) {
+            max = arr[i]; // Update max if current element is greater
+        }
+    }
+    return max;
 }
+
+int main() {
+    int myArray[] = {2, 4, 6, 3, 7, 5};
+    int length = sizeof(myArray) / sizeof(myArray[0]); // Calculate the length of the array
+
+    // Print the length of the array
+    printf("The length of the array is: %d\n", length);
+
+    // Print the elements of the array
+    printf("The elements of the array are: ");
+    int i;
+    for (i = 0; i < length; i++) {
+        printf("%d ", myArray[i]);
+    }
+    printf("\n"); // Move to the next line after printing all elements
+
+    // Find and print the maximum element in the array
+    int max = findMax(myArray, length);
+    printf("The maximum element in the array is: %d\n", max);
+
+    return 0;
+}
+
