6f6b878cddc04c86f586dbdd85619272a7b5cefd
Asingura Philip <asinguraphilip@gmail.com>
2024-10-12 10:38:47 +0300

    merge sort

---
+++ mergesort.py
@@ -0,0 +1,38 @@
+def merge_sort(array):
+    if len(array)>1:
+
+        mid=len(array)//2
+
+        left_half=array[:mid]
+        right_half=array[mid:]
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i=j=k=0
+
+        while i<len(left_half) and j<len(right_half):
+            if left_half[i]<right_half[j]:
+                array[k]=left_half[i]
+                i+=1
+            else:
+                array[k]=right_half[j]
+                j+=1
+
+            k+=1
+
+        while i<len(left_half):
+            array[k]=left_half[i]
+            i+=1
+            k+=1
+        while j<len(right_half):
+            array[k]=right_half[j]
+            j+=1
+            k+=1
+
+if __name__=="__main__":
+    array=[22,18,-4,-7,28,1230,4059,-1,12,12,22,134]
+    print("Original array:",array)
+
+    merge_sort(array)
+
+    print("Sorted Array",array)            
