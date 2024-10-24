d6a61970db05bb609840b5bb521426a8b10c3e0c
Asingura Philip <asinguraphilip@gmail.com>
2024-10-24 21:38:44 +0300

    marks

--- classwork.py
+++ classwork.py
@@ -1,6 +1,19 @@
 def grade(mark):
     if mark < 0:
-        print("enter positive integer")
+        print("Enter a positive integer.")
     elif mark < 50:
-        print(f"Failed.. marks :{mark}")
-    elif
+        print(f"Failed.. marks: {mark}")
+    elif mark < 60:
+        print(f"Grade: D.. marks: {mark}")
+    elif mark < 70:
+        print(f"Grade: C.. marks: {mark}")
+    elif mark < 80:
+        print(f"Grade: B.. marks: {mark}")
+    elif mark < 90:
+        print(f"Grade: A.. marks: {mark}")
+    else:
+        print(f"Grade: A+.. marks: {mark}")
+
+
+grade(45)  
+grade(75)  
