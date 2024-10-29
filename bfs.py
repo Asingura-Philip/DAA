6d5b25c204a954f4d21ec0672c10d0eb84966c36
Asingura Philip <asinguraphilip@gmail.com>
2024-10-29 16:02:52 +0300

    breadth-first implementation

---
+++ bfs.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs(graph,start):
+    visited=set()
+    queue=deque([start])
+    output=[]
+
+    while queue:
+        node=queue.popleft()
+        if node not in visited:
+            print(node,end="")
+            visited.add(node)
+            output.append(node)
+
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+    return output
+
+graph={
+    "A":["S","D"],
+    "B":["D","S"],
+    "S":["A","B","C"],
+    "D":["A","B","C"],
+    "C":["S","D"],
+}
+bfs(graph,"S")
