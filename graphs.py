76bf05fc2498e57d69ba138015968875b0f7e1ea
Asingura Philip <asinguraphilip@gmail.com>
2024-10-29 15:00:30 +0300

    graph implementation

---
+++ graphs.py
@@ -0,0 +1,28 @@
+graph = {}
+
+def add_node(graph,node):
+    if node not in graph:
+        graph[node]= []
+
+        
+def add_edge(graph,node1,node2):
+    if node1 in graph and node2 in graph:
+        graph[node1].append(node2)
+        graph[node2].append(node1)
+    else:
+        print("one or more nodes are not in the graph")
+
+def display_graph(graph):
+    for node,edges in graph.items():
+        print(f"{node}:{edges}")
+
+add_node(graph,"A")
+add_node(graph,"B")
+add_node(graph,"C")
+add_node(graph,"D")
+
+add_edge(graph,"A","B")
+add_edge(graph,"A","c")
+add_edge(graph,"B","C")
+add_edge(graph,"C","D")
+display_graph(graph)
